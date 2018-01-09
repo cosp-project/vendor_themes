8cf68d1e440e4afa4181cdc8271b43e5a149ad37
Joey <joey@lineageos.org>
2018-01-09 13:40:56 +0100

    Import accents packages
    @jhenrique09 edits: Adapt to PE
    
    commit e516bf9c95a59bfc62098d1e50b5c83accff4f28
    Author: Joey <joey@lineageos.org>
    Date:   Sat Dec 8 10:55:43 2018 +0100
    
        overlays: accents: tune for contrast
    
        Tune some colors for improved contrast on both light and dark backgrounds
    
        Signed-off-by: Joey <joey@lineageos.org>
        Change-Id: Ida8d0d2622ec6384fe3a2328d4b1a179f482f106
    
    commit fb30540870d2dddf2fbcb6f17fe62e7889547e8b
    Author: Sam Mortimer <sam@mortimer.me.uk>
    Date:   Wed Aug 22 23:28:58 2018 -0700
    
        overlays/Lineage: Set LOCAL_PRIVATE_PLATFORM_APIS := true
    
        *) Apps on pie have to set LOCAL_SDK_VERSION or
           LOCAL_PRIVATE_PLATFORM_APIS
    
        *) Overlays are platform apps so set platform APIs
    
        *) Unbreaks build on pie
    
        Change-Id: I77ce4dbc53846dd617dec3dbab475253ad7689e3
    
    Signed-off-by: Joey <joey@lineageos.org>
    Signed-off-by: Henrique Silva <jhenrique09.mcz@hotmail.com>
    Change-Id: Iaa15613acf561a99365de56f83fc52a30e6b3f98

--- config.mk
+++ config.mk
@@ -2,3 +2,17 @@ PRODUCT_PACKAGES += \
     SettingsDark \
     SystemDark \
     SystemUIDark 
+
+# Accents
+PRODUCT_PACKAGES += \
+    BrownAccent \
+    TealAccent \
+    BlackAccent \
+    YellowAccent \
+    RedAccent \
+    PurpleAccent \
+    PinkAccent \
+    OrangeAccent \
+    GreenAccent \
+    WhiteAccent \
+    CyanAccent \
